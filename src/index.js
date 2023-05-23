1fc31682f5d1bf7da081a3de073cef14daf24b46
Aiden Luce <luceaiden4124@gmail.com>
2023-05-23 14:50:24 -0500

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function SanityCheck(){
+    return(
+        <section>
+            <h3>It Works!</h3>
+        </section>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<SanityCheck/>)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
